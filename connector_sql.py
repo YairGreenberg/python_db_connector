ca49082a0e8ac9c001c84f286442240ada8ac686
yair greenberg <yair290899@gmail.com>
2025-11-25 10:20:48 +0200

    conneted to db

---
+++ connector_sql.py
@@ -0,0 +1,30 @@
+import mysql.connector
+from mysql.connector import connect
+
+
+
+connected = mysql.connector.connect(
+        user="root",
+        password="",
+        host='127.0.0.1',
+        database="classicmodels",
+)
+print("connected, server version: ",connected.get_server_info())
+
+
+cursor = connected.cursor()
+cursor.execute("SELECT * FROM customers")
+
+
+
+data = cursor.fetchall()
+print(data)
+
+
+
+
+
+
+
+
+
